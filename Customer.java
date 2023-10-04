266519dcf51e205aa354dccbb1f2cff7ea1f7d35
Yulia Romppainen <2003982@edu.karelia.fi>
2023-10-04 21:44:58 +0300

    Bank class, Customer object added

---
+++ Customer.java
@@ -0,0 +1,12 @@
+public class Customer {
+    
+    private String name;
+    private String accountNumber;
+
+    public Customer (String name, String accountNumber){
+
+        this.name = name;
+        this.accountNumber = accountNumber;
+    }
+
+}
